73c876574474dd0ce98db83c4715d4228f3b123a
arach-code <aarach72@gmail.com>
2025-09-14 04:22:49 +0300

    add users

--- controllers/users.js
+++ controllers/users.js
@@ -2,13 +2,9 @@ const { param } = require('../../routes');
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
-const getAll = async (req, res) => {
-     const result = await mongodb.getDatabase().db().collection('users').find();
-    result.toArray().then((users) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users);
-    })
-};
+const getAllUsers = (req, res) => {
+  res.send("All users endpoint working");
+    };
 
 const getSingle = async (req, res) => {
     const userId = new ObjectId(req,param.id);
@@ -19,7 +15,4 @@ const getSingle = async (req, res) => {
     })
 };
 
-module.exports = {
-    getAll,
-    getSingle
-};
+module.exports = { getAllUsers };
